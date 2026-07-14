a676d3d56b7951449c05f12544440bbc79fb9b14
ClaudiaAlfieri <claudia.jorge86@gmail.com>
2026-07-14 13:22:05 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Página Home - visitantes
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name('home');
+
+// Login - visitantes
+Route::get('/login', function () {
+    return view('login');
+})->name('login');
+
+// Dashboard - utilizadores autenticados
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
